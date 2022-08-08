af380df5b9b3e46bb0e2e01503039a6568a60f35
Altin <107879235+Altinzzzzz@users.noreply.github.com>
2022-08-08 15:33:36 +0200

    Add files via upload

--- LibraryLogic.js
+++ LibraryLogic.js
@@ -1,11 +1,20 @@
 let myLibrary = [];
+var body = document.querySelector('body');
+var main = document.querySelector('main');
+var container = document.querySelector('.container');
 var form = document.querySelector('#btnsubmit');
 var showBtn = document.querySelector('.activeBtn');
 var mainForm = document.querySelector('form');
+var librarySection = document.querySelector('.container');
 
 showBtn.addEventListener('click', () => {
     mainForm.classList.toggle('hideClass');
-    console.log(mainForm.classList);
+    
+    if(mainForm.className == 'hideClass'){
+        main.style.opacity = '1';
+    } else {
+        main.style.opacity = '0.5';
+    }
 });
 
 function Book(author, title, numberOfPages){
@@ -15,17 +24,40 @@ function Book(author, title, numberOfPages){
 }
 
 form.addEventListener('click', () => {
+    const author = document.querySelector('#author').value;
+    const title = document.querySelector('#title').value;
+    const numberOP = document.querySelector('#numberOfPages').value;
+
+    addBookToLibrary(author, title, numberOP);
+});
+
+function addBookToLibrary(author, title, numberOP){
+
+    myLibrary.push(new Book(
+        author,
+        title,
+        numberOP
+    ));
+
+    updatePage(author, title, numberOP);
+    
+    console.log(myLibrary);
     console.log(document.querySelector('#author').value);
     console.log(document.querySelector('#title').value);
     console.log(document.querySelector('#numberOfPages').value);
-
-    myLibrary.push(new Book(
-        document.querySelector('#author').value,
-        document.querySelector('#title').value,
-        document.querySelector('#numberOfPages').value));
-    console.log(myLibrary);
-});
-
-function addBookToLibrary(){
-
 }
+
+function updatePage(author, title, numberOP){
+    let mainCell = document.createElement('div');
+    let authorChild = document.createElement('p');
+    let titleChild = document.createElement('p');
+    let numberOPChild = document.createElement('p');
+
+    mainCell.appendChild(authorChild).textContent = author;
+    mainCell.appendChild(titleChild).textContent = title;
+    mainCell.appendChild(numberOPChild).textContent = numberOP;
+    
+    container.appendChild(mainCell);
+}
+
+addBookToLibrary('ALTIN', 'SYLEJMANI', '22');
